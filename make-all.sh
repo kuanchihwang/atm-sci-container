b9abed29484a282dd93b71b5ff33c233e6bb54b7
Kuan-Chih Wang <kuanchihwang@outlook.com>
2026-01-30 03:51:18 +0800

    Rearrange build order to improve caching
    Complete a loop on compilers first so subsequent builds can just
    start from cached results.

--- make-all.sh
+++ make-all.sh
@@ -8,13 +8,13 @@ echo "Container build began at $(date -u "+%FT%TZ")"
 TARGET="${1:-build}"
 VERSION="$(date -u "+%F")"
 
-for COMPILER in "gcc-11" "gcc-12" "gcc-13" "gcc-14" "intel-2024" "intel-2025"; do
-    for MPI in "intel-mpi" "mpich-4" "open-mpi-4" "open-mpi-5"; do
+for MPI in "mpich-4" "open-mpi-4" "open-mpi-5" "intel-mpi"; do
+    for COMPILER in "gcc-11" "gcc-12" "gcc-13" "gcc-14" "gcc-15" "intel-2024" "intel-2025"; do
         # Filter out invalid combinations.
-        case "${COMPILER}" in
-            gcc-*)
-                case "${MPI}" in
-                    intel-mpi)
+        case "${MPI}" in
+            intel-mpi)
+                case "${COMPILER}" in
+                    gcc-*)
                         continue
                         ;;
                 esac
