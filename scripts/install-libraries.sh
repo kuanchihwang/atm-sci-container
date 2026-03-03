c978d8c405634bac03db7617d72aade5ce64ca4d
Kuan-Chih Wang <kuanchihwang@outlook.com>
2026-03-04 05:55:05 +0800

    Update libaec to 1.1.6

--- scripts/install-libraries.sh
+++ scripts/install-libraries.sh
@@ -52,10 +52,10 @@ compile_and_install_libaec() {
     fi
 
     echo ">>>>> Preparing libaec"
-    if [ ! -d libaec-1.1.5 ]; then
-        extract_archive "${LIBRARIES_PATH}/libaec-1.1.5.tar.gz"
+    if [ ! -d libaec-1.1.6 ]; then
+        extract_archive "${LIBRARIES_PATH}/libaec-1.1.6.tar.gz"
     fi
-    stage_build_directory libaec-1.1.5
+    stage_build_directory libaec-1.1.6
 
     echo ">>>>> Configuring libaec"
     ../source/configure --help
